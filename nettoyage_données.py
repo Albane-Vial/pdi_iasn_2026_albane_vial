143b760f974cbffaba2a3d9afc47bab91ad605e8
albane_vial <131970443+Albane-Vial@users.noreply.github.com>
2026-03-18 15:41:53 +0100

    Enhance data extraction with print statements
    Updated the data extraction and cleaning functions to include print statements for better tracking of the process. Changed the data path for loading datasets.

--- nettoyage_données.py
+++ nettoyage_données.py
@@ -9,14 +9,13 @@ from torch.optim import AdamW
 from sklearn.model_selection import train_test_split
 
 
-
-
-DATA_PATH = '/kaggle/input/mimic-iv-2-1/mimic-iv-2.1/hosp/'
+DATA_PATH = '/kaggle/input/datasets/mangeshwagle/mimic-iv-2-1/mimic-iv-2.1/hosp/'
 
 print("Chargement des prescriptions MIMIC en cours...")
 
 
 def extraire_donnée_precriptions():
+    print("Extraction et nettoyage des données de prescriptions...")
     df_prescriptions = pd.read_csv(DATA_PATH + 'prescriptions.csv', nrows=500000, low_memory=False)
     colonnes_utiles = ['hadm_id','subject_id','drug', 'dose_val_rx', 'dose_unit_rx', 'form_val_disp', 'form_unit_disp', 'route']
     df_prescriptions = df_prescriptions[colonnes_utiles].dropna().copy()
@@ -27,27 +26,30 @@ def extraire_donnée_precriptions():
     df_prescriptions['dose_val_rx'] = pd.to_numeric(df_prescriptions['dose_val_rx'], errors='coerce')
     quantiles = df_prescriptions.groupby('drug')['dose_val_rx'].quantile([0.01, 0.99]).unstack()
     quantiles.columns = ['q01', 'q99']
-    df_traitement['q01'] = df_traitement['drug'].map(quantiles['q01'])
-    df_traitement['q99'] = df_traitement['drug'].map(quantiles['q99'])
+    df_prescriptions['q01'] = df_prescriptions['drug'].map(quantiles['q01'])
+    df_prescriptions['q99'] = df_prescriptions['drug'].map(quantiles['q99'])
 
-    df_traitement = df_traitement[
-        (df_traitement['dose_val_rx'] >= df_traitement['q01']) & 
-        (df_traitement['dose_val_rx'] <= df_traitement['q99'])
+    df_prescriptions = df_prescriptions[
+        (df_prescriptions['dose_val_rx'] >= df_prescriptions['q01']) & 
+        (df_prescriptions['dose_val_rx'] <= df_prescriptions['q99'])
     ]
     print(f"Nombre de prescriptions après nettoyage : {len(df_prescriptions)}")
     df_prescriptions.drop(columns=['q01', 'q99'], inplace=True)
     return df_prescriptions
 def extraire_donnée_patients():
+    print("Extraction et nettoyage des données de patients...")
     df_patients = pd.read_csv(DATA_PATH + 'patients.csv', low_memory=False)
     colonnes_utiles = ['subject_id', 'gender', 'anchor_age']
     df_patients = df_patients[colonnes_utiles].dropna().copy()
     return df_patients
 def extraire_donnée_admissions():
+    print("Extraction et nettoyage des données d'admissions...")
     df_admissions = pd.read_csv(DATA_PATH + 'admissions.csv', low_memory=False)
     colonnes_utiles = ['hadm_id', 'subject_id', 'admission_type']
     df_admissions = df_admissions[colonnes_utiles].dropna().copy()
     return df_admissions
 def extraire_donnée_diagnostics():
+    print("Extraction et nettoyage des données de diagnostics...")
     df_diag = pd.read_csv(DATA_PATH + 'diagnoses_icd.csv')
     df_diag_name = pd.read_csv(DATA_PATH + 'd_icd_diagnoses.csv')
     df_diag_filtre = df_diag[df_diag['seq_num'] <= 2]
@@ -69,6 +71,7 @@ def extraire_donnée_diagnostics():
     })
     return df_diag_merged
 def extraire_donnée_biologie():
+    print("Extraction et nettoyage des données de biologie...")
     items_cibles = [50912, 50971, 51265, 51274, 51275, 50931, 50861, 50878]
 
     df_lab_bio = pd.read_csv(DATA_PATH + 'labevents.csv', usecols=['subject_id', 'hadm_id', 'itemid', 'flag'])
@@ -112,6 +115,8 @@ def generer_phrase(row):
             f"[BIO] {row['nom_bio']}")
 
 def executer_pipeline_nettoyage():
+    print("Démarrage du nettoyage...")
+
     df_prescriptions = extraire_donnée_precriptions()
     df_patients = extraire_donnée_patients()
     df_admissions = extraire_donnée_admissions()
@@ -127,6 +132,7 @@ def executer_pipeline_nettoyage():
     df_final.rename(columns={'label': 'nom_bio'}, inplace=True)
 
     df_final = df_final.drop_duplicates()
+    print(f"Génération de la phrase clinique")
     df_final['phrase_clinique'] = df_final.apply(generer_phrase, axis=1)
 
     return df_final
